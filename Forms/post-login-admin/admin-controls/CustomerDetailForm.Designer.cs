ba94bd66fc729d58897789eebad934d9dbf0477e
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2024-08-11 18:55:41 +0530

    create customer page with view option

---
+++ Forms/post-login-admin/admin-controls/CustomerDetailForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ABCTradersApp.Forms.post_login_admin.admin_controls
+{
+    partial class CustomerDetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblNIC = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblTotalOrders = new System.Windows.Forms.Label();
+            this.lblCustomerIDValue = new System.Windows.Forms.Label();
+            this.lblFirstNameValue = new System.Windows.Forms.Label();
+            this.lblLastNameValue = new System.Windows.Forms.Label();
+            this.lblEmailValue = new System.Windows.Forms.Label();
+            this.lblPhoneValue = new System.Windows.Forms.Label();
+            this.lblNICValue = new System.Windows.Forms.Label();
+            this.lblAddressValue = new System.Windows.Forms.Label();
+            this.lblTotalOrdersValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Location = new System.Drawing.Point(30, 30);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(83, 16);
+            this.lblCustomerID.TabIndex = 0;
+            this.lblCustomerID.Text = "Customer ID:";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(30, 70);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(75, 16);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(30, 110);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(75, 16);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(30, 150);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(30, 190);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(49, 16);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // lblNIC
+            // 
+            this.lblNIC.AutoSize = true;
+            this.lblNIC.Location = new System.Drawing.Point(30, 230);
+            this.lblNIC.Name = "lblNIC";
+            this.lblNIC.Size = new System.Drawing.Size(32, 16);
+            this.lblNIC.TabIndex = 12;
+            this.lblNIC.Text = "NIC:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(30, 270);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(61, 16);
+            this.lblAddress.TabIndex = 14;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblTotalOrders
+            // 
+            this.lblTotalOrders.AutoSize = true;
+            this.lblTotalOrders.Location = new System.Drawing.Point(30, 310);
+            this.lblTotalOrders.Name = "lblTotalOrders";
+            this.lblTotalOrders.Size = new System.Drawing.Size(85, 16);
+            this.lblTotalOrders.TabIndex = 5;
+            this.lblTotalOrders.Text = "Total Orders:";
+            // 
+            // lblCustomerIDValue
+            // 
+            this.lblCustomerIDValue.AutoSize = true;
+            this.lblCustomerIDValue.Location = new System.Drawing.Point(150, 30);
+            this.lblCustomerIDValue.Name = "lblCustomerIDValue";
+            this.lblCustomerIDValue.Size = new System.Drawing.Size(0, 16);
+            this.lblCustomerIDValue.TabIndex = 6;
+            // 
+            // lblFirstNameValue
+            // 
+            this.lblFirstNameValue.AutoSize = true;
+            this.lblFirstNameValue.Location = new System.Drawing.Point(150, 70);
+            this.lblFirstNameValue.Name = "lblFirstNameValue";
+            this.lblFirstNameValue.Size = new System.Drawing.Size(0, 16);
+            this.lblFirstNameValue.TabIndex = 7;
+            // 
+            // lblLastNameValue
+            // 
+            this.lblLastNameValue.AutoSize = true;
+            this.lblLastNameValue.Location = new System.Drawing.Point(150, 110);
+            this.lblLastNameValue.Name = "lblLastNameValue";
+            this.lblLastNameValue.Size = new System.Drawing.Size(0, 16);
+            this.lblLastNameValue.TabIndex = 8;
+            // 
+            // lblEmailValue
+            // 
+            this.lblEmailValue.AutoSize = true;
+            this.lblEmailValue.Location = new System.Drawing.Point(150, 150);
+            this.lblEmailValue.Name = "lblEmailValue";
+            this.lblEmailValue.Size = new System.Drawing.Size(0, 16);
+            this.lblEmailValue.TabIndex = 9;
+            // 
+            // lblPhoneValue
+            // 
+            this.lblPhoneValue.AutoSize = true;
+            this.lblPhoneValue.Location = new System.Drawing.Point(150, 190);
+            this.lblPhoneValue.Name = "lblPhoneValue";
+            this.lblPhoneValue.Size = new System.Drawing.Size(0, 16);
+            this.lblPhoneValue.TabIndex = 10;
+            // 
+            // lblNICValue
+            // 
+            this.lblNICValue.AutoSize = true;
+            this.lblNICValue.Location = new System.Drawing.Point(150, 229);
+            this.lblNICValue.Name = "lblNICValue";
+            this.lblNICValue.Size = new System.Drawing.Size(0, 16);
+            this.lblNICValue.TabIndex = 13;
+            // 
+            // lblAddressValue
+            // 
+            this.lblAddressValue.AutoSize = true;
+            this.lblAddressValue.Location = new System.Drawing.Point(150, 269);
+            this.lblAddressValue.Name = "lblAddressValue";
+            this.lblAddressValue.Size = new System.Drawing.Size(0, 16);
+            this.lblAddressValue.TabIndex = 15;
+            // 
+            // lblTotalOrdersValue
+            // 
+            this.lblTotalOrdersValue.AutoSize = true;
+            this.lblTotalOrdersValue.Location = new System.Drawing.Point(150, 310);
+            this.lblTotalOrdersValue.Name = "lblTotalOrdersValue";
+            this.lblTotalOrdersValue.Size = new System.Drawing.Size(0, 16);
+            this.lblTotalOrdersValue.TabIndex = 11;
+            // 
+            // CustomerDetailForm
+            // 
+            this.ClientSize = new System.Drawing.Size(544, 368);
+            this.Controls.Add(this.lblNIC);
+            this.Controls.Add(this.lblNICValue);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblAddressValue);
+            this.Controls.Add(this.lblCustomerID);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblTotalOrders);
+            this.Controls.Add(this.lblCustomerIDValue);
+            this.Controls.Add(this.lblFirstNameValue);
+            this.Controls.Add(this.lblLastNameValue);
+            this.Controls.Add(this.lblEmailValue);
+            this.Controls.Add(this.lblPhoneValue);
+            this.Controls.Add(this.lblTotalOrdersValue);
+            this.Name = "CustomerDetailForm";
+            this.Text = "Customer Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
--- Forms/post-login-admin/admin-controls/CustomerManagementControl.Designer.cs
+++ Forms/post-login-admin/admin-controls/CustomerManagementControl.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ABCTradersApp.Forms.post_login_admin.admin_controls
+﻿using System.Windows.Forms;
+using System;
+
+namespace ABCTradersApp.Forms.post_login_admin.admin_controls
 {
     partial class CustomerManagementControl
     {
@@ -28,8 +31,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridViewCustomers = new System.Windows.Forms.DataGridView();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(81, 18);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(300, 22);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.TxtSearch_TextChanged);
+            // 
+            // dataGridViewCustomers
+            // 
+            this.dataGridViewCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomers.Location = new System.Drawing.Point(20, 60);
+            this.dataGridViewCustomers.Name = "dataGridViewCustomers";
+            this.dataGridViewCustomers.RowHeadersWidth = 51;
+            this.dataGridViewCustomers.RowTemplate.Height = 24;
+            this.dataGridViewCustomers.Size = new System.Drawing.Size(800, 400);
+            this.dataGridViewCustomers.TabIndex = 1;
+            this.dataGridViewCustomers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridViewCustomers_CellClick);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(19, 21);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(56, 16);
+            this.lblSearch.TabIndex = 2;
+            this.lblSearch.Text = "Search :";
+            // 
+            // CustomerManagementControl
+            // 
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dataGridViewCustomers);
+            this.Name = "CustomerManagementControl";
+            this.Size = new System.Drawing.Size(850, 500);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
